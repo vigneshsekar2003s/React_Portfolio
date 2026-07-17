30435812b8b9f654724dae3c3022e18e5e4f8c22
vigneshsekar2003s <vigneshsekar2003s@gmail.com>
2026-07-17 22:34:16 +0530

    updated the SEO

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,38 +1,46 @@
 import "./Loader.css";
 import { motion } from "framer-motion";
+import Icon2 from "../../assets/image/Icon2.jpeg";
+// import VSLogo from "../../assets/image/VS-Logo.jpeg";
 
 function Loader() {
   return (
     <div className="loader">
 
-      <motion.h1
-        initial={{ scale: 0 }}
-        animate={{ scale: 1 }}
+      {/* VS Logo */}
+      <motion.img
+        // src={VSLogo}
+        // alt="VS Logo"
+        src={Icon2}
+        alt="Icon2"
+        className="loader-logo"
+        initial={{ scale: 0, opacity: 0 }}
+        animate={{ scale: 1, opacity: 1 }}
         transition={{
           duration: 0.8,
           type: "spring",
         }}
-      >
-        VS
-      </motion.h1>
+      />
 
+      {/* Portfolio Text */}
       <motion.h2
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
+        initial={{ opacity: 0, y: 20 }}
+        animate={{ opacity: 1, y: 0 }}
         transition={{
           delay: 0.6,
           duration: 0.6,
         }}
       >
-        PORTFOLIO
+        VIGNESH SEKAR
       </motion.h2>
 
+      {/* Loading Line */}
       <motion.div
         className="loading-bar"
         initial={{ width: 0 }}
         animate={{ width: "220px" }}
         transition={{
-          duration: 2,
+          duration: 2.5,
         }}
       />
 
