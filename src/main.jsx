720ec19b5e549dd19a0db3ed788163d9e4deafe7
vigneshsekar2003s <vigneshsekar2003s@gmail.com>
2026-08-05 13:14:14 +0530

    Add vercel Analytics

--- src/main.jsx
+++ src/main.jsx
@@ -6,10 +6,13 @@ import "./index.css";
 
 import ThemeProvider from "./context/ThemeContext";
 
+import { Analytics } from "@vercel/analytics/react";
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ThemeProvider>
       <App />
+      <Analytics />
     </ThemeProvider>
   </React.StrictMode>
 );
