d3bec887c265cd48e253f8c6a8b234111ae204e4
vigneshsekar2003s <vigneshsekar2003s@gmail.com>
2026-08-05 13:18:55 +0530

    Add Speed Insights

--- src/main.jsx
+++ src/main.jsx
@@ -8,11 +8,14 @@ import ThemeProvider from "./context/ThemeContext";
 
 import { Analytics } from "@vercel/analytics/react";
 
+import { SpeedInsights } from "@vercel/speed-insights/react";
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ThemeProvider>
       <App />
       <Analytics />
+      <SpeedInsights />
     </ThemeProvider>
   </React.StrictMode>
 );
