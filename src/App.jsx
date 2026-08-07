5df76a244ba226bf97e58def55c46359b680ed42
vigneshsekar2003s <vigneshsekar2003s@gmail.com>
2026-08-07 20:26:30 +0530

    update animations secs

--- src/App.jsx
+++ src/App.jsx
@@ -27,7 +27,7 @@ function App() {
   useEffect(() => {
     const timer = setTimeout(() => {
       setLoading(false);
-    }, 2000);
+    }, 1000);
 
     return () => clearTimeout(timer);
   }, []);
